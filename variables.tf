6338d2031b65c6baf79d6631b980683bdb9b9378
rbikai <rabih.bikai@teolia.fr>
2023-09-08 16:12:26 +0200

    Clean commit

--- variables.tf
+++ variables.tf
@@ -37,19 +37,19 @@ variable "port" {
 variable "cpu_cores" {
   type        = number
   description = "The number of CPU cores to allocate to the container."
-  default     = 1
+  default     = 2
 }
 
 variable "memory_in_gb" {
   type        = number
   description = "The amount of memory to allocate to the container in gigabytes."
-  default     = 2
+  default     = 8
 }
 
 variable "restart_policy" {
   type        = string
   description = "The behavior of Azure runtime if container has stopped."
-  default     = "Always"
+  default     = "OnFailure"
   validation {
     condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
     error_message = "The restart_policy must be one of the following: Always, Never, OnFailure."
